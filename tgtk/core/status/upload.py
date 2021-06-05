c88838cc19ce31cebeafe447502f53d1c0498ce4
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-05 13:43:29 +0530

    Update upload.py

--- tgtk/core/status/upload.py
+++ tgtk/core/status/upload.py
@@ -74,7 +74,7 @@ class TGUploadTask(Status):
         self._current_file = str(name)
 
     async def create_message(self):
-        msg = "<b>uploading:- </b> <code>{}</code>\n".format(
+        msg = "<b>━━┫ Uploading 📤 ┣━━</b>\n\n🗃️ File Name:  <code>{}</code>\n".format(
             self._current_file
         )
         prg = 0
@@ -82,15 +82,15 @@ class TGUploadTask(Status):
             prg = self._uploaded_files/self._files
 
         except ZeroDivisionError:pass
-        msg += "<b>progress: </b> {} - {}%\n".format(
+        msg += "<b>📡  Progress:</b> {} - {}%\n".format(
             self.progress_bar(prg),
             prg*100
         )
-        msg += "<b>files: </b> {} of {} done.\n".format(
+        msg += "<b>📁 Files:</b> {} of {} done.\n".format(
             self._uploaded_files,
             self._files
         )
-        msg += "<b>using engine: </b> <code>telegram upload</code>\n"
+        msg += "<b>💠 Using Engine:- </b> <code>[ TG Upload ]</code>\n"
         return msg
 
     def progress_bar(self, percentage):
