2b963eedd9e678ea56b5e51ea4552009c2bb7cf8
Dlaize Kohli <dakshkohli23@gmail.com>
2021-06-05 23:39:35 +0530

    Update progress_for_telethon.py

--- tgtk/functions/progress_for_telethon.py
+++ tgtk/functions/progress_for_telethon.py
@@ -18,7 +18,7 @@ async def progress(current,total,message,file_name,start,time_out,cancel_msg=Non
             # dirty alt. was not able to find something to stop upload
             # todo inspect with "StopAsyncIteration"
             if updb.get_cancel_status(cancel_msg.chat_id,cancel_msg.id):
-                raise Exception("cancel the upload")
+                raise Exception("✖️ Cancel the upload")
 
         # if round(current / total * 100, 0) % 5 == 0:
         percentage = current * 100 / total
@@ -32,12 +32,12 @@ async def progress(current,total,message,file_name,start,time_out,cancel_msg=Non
         estimated_total_time = human_readable_timedelta(seconds=estimated_total_time/1000)
 
 
-        progress = "[{0}{1}] \nP: {2}%\n".format(
+        progress = "【{0}{1}】\n📡  Progress: {2}%\n".format(
             ''.join([get_val("COMPLETED_STR") for i in range(math.floor(percentage / 10))]),
             ''.join([get_val("REMAINING_STR") for i in range(10 - math.floor(percentage / 10))]),
             round(percentage, 2))
         
-        tmp = progress + "{0} of {1}\nSpeed: {2}/s\nETA: {3}\nusing engine: telethon".format(
+        tmp = progress + "{0} of {1}\n🚀 Speed: {2}/s\n🕰️ ETA: {3}\n<b>\n💠 Using Engine:</b> <code>[ Telethon ]</code>\n".format(
             human_readable_bytes(current),
             human_readable_bytes(total),
             human_readable_bytes(speed),
@@ -47,14 +47,14 @@ async def progress(current,total,message,file_name,start,time_out,cancel_msg=Non
         try:
             if not message.photo:
                 await message.edit(
-                    text="**Uploading:** `{}`\n{}".format(
+                    text="**📤 Uploading:** `{}`\n{}".format(
                         file_name,
                         tmp
                     )
                 )
             else:
                 await message.edit(
-                    caption="**Uploading:** `{}`\n{}".format(
+                    caption="**📤 Uploading:** `{}`\n{}".format(
                         file_name,
                         tmp
                     )
