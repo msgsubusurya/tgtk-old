e33bbb2532fb44ac630a1deec5f6fe3b949d6b5b
Dlaize Kohli <dakshkohli23@gmail.com>
2021-07-20 07:42:12 +0530

    Update Leech_Module.py

--- tgtk/functions/Leech_Module.py
+++ tgtk/functions/Leech_Module.py
@@ -127,8 +127,7 @@ async def check_link(msg,rclone=False,is_zip=False, extract=False, prev_msg=None
                         torlog.exception("Exception in Torrent file.")
 
                     await ul_task.set_inactive()
-                    print_files(omess,rdict,dl_task.hash, path = dl_path, size=ul_size)
-                    #await print_files(omess,rdict, path = path, size=ul_size)
+                    await print_files(omess,rdict,dl_task.hash, path = dl_path, size=ul_size)
                     torlog.info("Here are the fiels uploaded {}".format(rdict))
                     await QBittorrentWrap.delete_this(dl_task.hash)
                 else:
